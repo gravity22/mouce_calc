85308aa183a2a0ffb54cc32a27e75baf80ad0260
teacat772 <teacat722@gmail.com>
2019-12-25 09:55:25 +0900

    distanceグラフのylimをべた書き

--- mouse_calc/ui/distancegraphpagewidget.py
+++ mouse_calc/ui/distancegraphpagewidget.py
@@ -22,6 +22,11 @@ class DistanceGraphPageWidget(GraphPageWidget):
         x_data = data.get_col(TIME)
         y_data = data.get_col(DISTANCE)
         self.appendData(x_data, y_data, "data", self.graphoptions)
+
+        data = DataManager.get_data(self.data_id)
+        x_data = data.get_col(TIME)
+        self.xlim = (min(x_data), max(x_data))
+        self.ylim = (0, 150)
         self.updateGraph()
 
         self.loadConfigSiganl.connect(self.reload_config)
